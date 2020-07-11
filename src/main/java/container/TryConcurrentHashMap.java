f633223b99b8a1c1181a179e42342229009a6178
Terence-cn <1035260902@qq.com>
2020-07-11 21:06:13 +0800

    ConcurrentHashMap

---
+++ src/main/java/container/TryConcurrentHashMap.java
@@ -0,0 +1,9 @@
+package container;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TryConcurrentHashMap {
+    public static void main(String[] args){
+        ConcurrentHashMap<Integer,Integer> concurrentHashMap = new ConcurrentHashMap<Integer, Integer>();
+    }
+}
