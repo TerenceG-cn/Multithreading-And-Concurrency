9bd8ac714843905d1920bf14d80aac666b8c880b
Terence-cn <1035260902@qq.com>
2020-07-18 17:34:32 +0800

    cacheUtils

---
+++ src/main/java/cas/AtomicBooleanTest.java
@@ -0,0 +1,44 @@
+package cas;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author hrabbit
+ * 2018/07/16.
+ */
+public class AtomicBooleanTest implements Runnable {
+
+    private static AtomicBoolean flag = new AtomicBoolean(true);
+
+    public static void main(String[] args) throws InterruptedException {
+        AtomicBooleanTest ast = new AtomicBooleanTest();
+        Thread thread0 = new Thread(ast);
+        Thread thread1 = new Thread(ast);
+        thread0.start();
+        flag.set(true);
+        Thread.sleep(1000);
+        thread1.start();
+    }
+    @Override
+    public void run() {
+        System.out.println("thread:"+Thread.currentThread().getName()+";flag:"+flag.get());
+        if (flag.compareAndSet(true,false)){
+            System.out.println(Thread.currentThread().getName()+""+flag.get());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            flag.set(true);
+        }else{
+            System.out.println("重试机制thread:"+Thread.currentThread().getName()+";flag:"+flag.get());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            run();
+        }
+
+    }
+}
--- src/main/java/container/TryConcurrentHashMap.java
+++ src/main/java/container/TryConcurrentHashMap.java
@@ -1,9 +1,12 @@
 package container;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TryConcurrentHashMap {
     public static void main(String[] args){
         ConcurrentHashMap<Integer,Integer> concurrentHashMap = new ConcurrentHashMap<Integer, Integer>();
+        Map<Integer,Integer> hashmap=new HashMap<Integer, Integer>();
     }
 }
